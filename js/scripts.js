50ad6ab89b09ee86690f4776ff3ad7fba0f7eaa9
Jason Ainsworth and Ken Rutan <ken@>
2017-03-20 16:55:03 -0700

    Add inital files to commit.

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+var answer="KRISTMAS";
+// var longstring = function() {
+//    answer = prompt("Tell me the names of the people in your family");
+// // var final = output.toUpperCase();
+// // alert(final);
+// // return final;
+// };
+//
+// longstring();
+console.log(answer);
+var result = answer.slice(0,1) + answer.slice(-1);
+
+var reverse = function(response){
+  alert(response);
+  response+=response;
+}
+reverse(result);
+reverse(jojoba);
+//
+
+
+
+
+// jQuery(document).ready(function() {
+//   jQuery("h1").click(function() {
+//     alert("This is a header.");
+//     alert("I told you, THIS IS A HEADER!");
+//   });
+//
+//   jQuery("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
+//
+//
+//
+//
+//   $("h3").click(function() {
+//     var answer = parseFloat(prompt("What is your lucky number?"));
+//     alert("My lucky number is: " + (answer+1));
+//   });
+//
+//   $("strong").click(function() {
+//     alert("This is a strong.");
+//
+//
+//   });
+//
+//
+//   jQuery("img").click(function() {
+//     alert("This is an image.");
+//   });
+//
+//   $("h3").click(function() {
+//     var name1 = prompt("What is your name?");
+//     var name2 = prompt("what is your name?");
+//     alert("Your pair group is "+ name1 + " " + name2);
+//   });
+// });
+//
+// var spitback = function(answer) {
+//   alert("Your ");
+// };
